83331cea83c86dfa4918e346e855244bb632a1d1
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-29 16:55:52 -0500

    Update chatbot.py

--- vector_librarian/pages/chatbot.py
+++ vector_librarian/pages/chatbot.py
@@ -5,14 +5,29 @@ import streamlit as st
 import client
 from authentication import openai_connection_status, weaviate_connection_status
 
-def retrieval_form_container(dr) -> None:
+def medical_search_prompt_container() -> None:
+    """Container to select a pre-programmed medical search prompt."""
+    st.subheader("Medical Search")
+    medical_prompt_options = [
+        "Diagnose a medical condition.",
+        "Treatment options for a specific disease.",
+        "Side effects of a medication.",
+        # Add more medical prompts as needed
+    ]
+    medical_prompt = st.selectbox("Select a medical prompt", medical_prompt_options)
+    return medical_prompt
+
+def retrieval_form_container(dr, is_medical_query) -> None:
     """Container to enter RAG query and send /rag_summary GET request."""
     left, right = st.columns(2)
     with left:
         form = st.form(key="retrieval_query")
-        rag_query = form.text_area(
-            "Retrieval Query", value="What are the main challenges of deploying ML models?"
-        )
+        if is_medical_query:
+            rag_query = medical_search_prompt_container()
+        else:
+            rag_query = form.text_area(
+                "Retrieval Query", value="What are the main challenges of deploying ML models?"
+            )
 
     with right:
         st.write("Hybrid Search Parameters")
@@ -41,49 +56,6 @@ def retrieval_form_container(dr) -> None:
             except Exception as e:
                 st.error(f"Error during retrieval: {str(e)}")
 
-def history_display_container(history):
-    if len(history) > 1:
-        st.header("History")
-        max_idx = len(history) - 1
-        history_idx = st.slider("History", 0, max_idx, value=max_idx, label_visibility="collapsed")
-        entry = history[history_idx]
-    else:
-        entry = history[0]
-
-    st.download_button(
-        "Download Q&A History",
-        data=json.dumps(history),
-        file_name="vector-librarian.json",
-        mime="application/json"
-    )
-
-    st.subheader("Query")
-    st.write(entry["query"])
-
-    st.subheader("Response")
-    st.write(entry["response"]["rag_summary"])
-
-    df = pd.DataFrame(entry["response"]["all_chunks"])
-    df = df.set_index("chunk_id")
-    df = df.rename(columns=dict(
-        score="Relevance",
-        chunk_index="Chunk Index",
-        document_file_name="File name",
-        content="Content",
-        summary="Summary"
-    ))
-
-    st.info("Double-click cell to read full content")
-    st.dataframe(
-        df,
-        hide_index=True,
-        use_container_width=True,
-        column_order=("Relevance", "Chunk Index", "File name", "Content", "Summary"),
-        column_config={col: st.column_config.Column(width="small") 
-                       for col in ("Relevance", "Chunk Index", "File name", "Content", "Summary")
-                      }
-    )
-
 def app() -> None:
     st.set_page_config(
         page_title="📤 retrieval",
@@ -107,7 +79,8 @@ def app() -> None:
     
     dr = client.instantiate_driver()
 
-    retrieval_form_container(dr)
+    is_medical_query = st.checkbox("Medical Search", value=False)
+    retrieval_form_container(dr, is_medical_query)
 
     if history := st.session_state.get("history"):
         history_display_container(history)
