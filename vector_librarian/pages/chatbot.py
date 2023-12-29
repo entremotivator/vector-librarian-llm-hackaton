4fe30fe0fe90d4a155f300534619014c95e52c39
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-29 17:03:26 -0500

    Update chatbot.py

--- vector_librarian/pages/chatbot.py
+++ vector_librarian/pages/chatbot.py
@@ -56,6 +56,49 @@ def retrieval_form_container(dr, is_medical_query) -> None:
             except Exception as e:
                 st.error(f"Error during retrieval: {str(e)}")
 
+def history_display_container(history):
+    if history:
+        st.header("History")
+        max_idx = len(history) - 1
+        history_idx = st.slider("History", 0, max_idx, value=max_idx, label_visibility="collapsed")
+        entry = history[history_idx]
+
+        st.download_button(
+            "Download Q&A History",
+            data=json.dumps(history),
+            file_name="vector-librarian.json",
+            mime="application/json"
+        )
+
+        st.subheader("Query")
+        st.write(entry["query"])
+
+        st.subheader("Response")
+        st.write(entry["response"]["rag_summary"])
+
+        df = pd.DataFrame(entry["response"]["all_chunks"])
+        df = df.set_index("chunk_id")
+        df = df.rename(columns=dict(
+            score="Relevance",
+            chunk_index="Chunk Index",
+            document_file_name="File name",
+            content="Content",
+            summary="Summary"
+        ))
+
+        st.info("Double-click cell to read full content")
+        st.dataframe(
+            df,
+            hide_index=True,
+            use_container_width=True,
+            column_order=("Relevance", "Chunk Index", "File name", "Content", "Summary"),
+            column_config={col: st.column_config.Column(width="small") 
+                           for col in ("Relevance", "Chunk Index", "File name", "Content", "Summary")
+                          }
+        )
+    else:
+        st.info("No history to display.")
+
 def app() -> None:
     st.set_page_config(
         page_title="📤 retrieval",
@@ -83,7 +126,7 @@ def app() -> None:
     retrieval_form_container(dr, is_medical_query)
 
     if history := st.session_state.get("history"):
-    
+        history_display_container(history)
     else:
         st.session_state["history"] = list()
 
