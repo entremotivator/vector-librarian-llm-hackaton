c1af17831aa44c45d4739e3413e7edbd7ce7fc8f
entremotivator <104661532+entremotivator@users.noreply.github.com>
2024-01-01 18:22:52 -0500

    Update chatbot.py

--- vector_librarian/pages/chatbot.py
+++ vector_librarian/pages/chatbot.py
@@ -2,7 +2,7 @@ import openai
 import json
 import pandas as pd
 import streamlit as st
-from trulens_eval import TruLens  # Import TruLens library
+from trulens import trulens-eval  # Import TruLens library
 
 import client
 from authentication import openai_connection_status, weaviate_connection_status
